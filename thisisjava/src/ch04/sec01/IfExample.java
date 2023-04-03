2caa79e79e07d5d539d4e39ac692994fe5ed2544
KOSA <KOSA@DESKTOP-RQQIKUK>
2023-04-03 17:21:05 +0900

    깃허브 연동

--- thisisjava/src/ch04/sec01/IfExample.java
+++ thisisjava/src/ch04/sec01/IfExample.java
@@ -1,5 +1,7 @@
 package ch04.sec01;
 
+import java.util.Scanner;
+
 public class IfExample {
 
 	public static void main(String[] args) {
@@ -10,8 +12,27 @@ public class IfExample {
 			System.out.println("점수가 90보다 큽니다.");
 			System.out.println("등급은 A입니다.");
 		}
-		if (score < 90)
+		if (score < 90) {
 			System.out.println("점수가 90보다 작습니다.");
-			System.out.println("등급은 B입니다.");	// 들여쓰기 오류 : if 문과는 상관없는 실행문
+		}
+		System.out.println("등급은 B입니다.");	// 들여쓰기 오류 : if 문과는 상관없는 실행문
+		
+		System.out.println("-----------------------------");
+		
+		Scanner scanner = new Scanner (System.in);
+		int score2 = scanner.nextInt();
+		if (score2 > 100 || score < 0) {
+			System.out.println("점수를 잘못 입력했습니다."	);
+		} else {
+			if (score2 >= 90) {
+				if (score2 >= 95) {
+					System.out.println("A+");
+				} else {
+					System.out.println("A");
+				}
+			}
+		}
+		scanner.close();
+		
 	}
 }
