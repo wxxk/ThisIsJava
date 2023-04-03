646ae43783ad2abf495572eda57cd24dcafcee2d
KOSA <KOSA@DESKTOP-RQQIKUK>
2023-04-03 17:04:02 +0900

    깃허브 연동

---
+++ thisisjava/src/ch04/sec01/IfExample.java
@@ -0,0 +1,17 @@
+package ch04.sec01;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int score = 93;
+		
+		if (score >= 90) {
+			System.out.println("점수가 90보다 큽니다.");
+			System.out.println("등급은 A입니다.");
+		}
+		if (score < 90)
+			System.out.println("점수가 90보다 작습니다.");
+			System.out.println("등급은 B입니다.");	// 들여쓰기 오류 : if 문과는 상관없는 실행문
+	}
+}
