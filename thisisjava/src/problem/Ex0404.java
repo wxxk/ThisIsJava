0cc860101c895fc5de953423c841681c25fe0038
wxxk <dwde22@gmail.com>
2023-04-04 18:12:26 +0900

    0404 숙제

---
+++ thisisjava/src/problem/Ex0404.java
@@ -0,0 +1,25 @@
+package problem;
+
+import java.util.Scanner;
+
+public class Ex0404 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		scanner.nextLine();
+		
+		for (int i = 0; i < t; i++) {
+			System.out.print("#" + (i+1) + " ");
+			int n = scanner.nextInt();
+			int sum = 0;
+			for (int j = 0; j < n; j++) {
+				int num = scanner.nextInt();
+				sum += num;
+			}
+			System.out.println(sum);
+		}
+	}
+
+}
