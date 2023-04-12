0c5657ad55c0c7b272e33440bebe1dec9e2f539e
wxxk <dwde22@gmail.com>
2023-04-12 12:11:11 +0900

    7.4 메소드 재정의

---
+++ thisisjava/src/ch07/sec04/exam02/SupersonicAirplaneExample.java
@@ -0,0 +1,17 @@
+package ch07.sec04.exam02;
+
+public class SupersonicAirplaneExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SupersonicAirplane sa = new SupersonicAirplane();
+		sa.takeOff();
+		sa.fly();
+		sa.flyMode = SupersonicAirplane.SUPERSONIC;
+		sa.fly();
+		sa.flyMode = SupersonicAirplane.NORMAL;
+		sa.fly();
+		sa.land();
+	}
+
+}
