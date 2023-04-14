e626026f77ac719d9a434298d02536be5cbebc24
wxxk <dwde22@gmail.com>
2023-04-14 18:30:32 +0900

    8.9 인터페이스 상속

---
+++ thisisjava/src/ch08/sec09/InterfaceCImpl.java
@@ -0,0 +1,25 @@
+package ch08.sec09;
+
+public class InterfaceCImpl implements InterfaceC {
+
+	@Override
+	public void methodA() {
+		// TODO Auto-generated method stub
+		System.out.println("InterfaceCImpl-methodA() 실행");
+	}
+
+	@Override
+	public void methodB() {
+		// TODO Auto-generated method stub
+		System.out.println("InterfaceCImpl-methodB() 실행");
+
+	}
+
+	@Override
+	public void methodC() {
+		// TODO Auto-generated method stub
+		System.out.println("InterfaceCImpl-methodC() 실행");
+
+	}
+
+}
