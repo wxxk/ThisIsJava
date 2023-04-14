2f22e6b16a06f6bcb86aabcb72a86d71a9617975
wxxk <dwde22@gmail.com>
2023-04-14 18:30:00 +0900

    8.8 다중 인터페이스 구현

---
+++ thisisjava/src/ch08/sec08/MultiInterfaceImplExample.java
@@ -0,0 +1,16 @@
+package ch08.sec08;
+
+public class MultiInterfaceImplExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RemoteControl rc = new SmartTelevision();
+		
+		rc.turnOn();
+		rc.turnOff();
+		
+		Searchable searchable = new SmartTelevision();
+		searchable.search("https://www.youtube.com");
+	}
+
+}
