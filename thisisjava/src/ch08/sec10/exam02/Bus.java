0d8aab9e2cb505007025ed5aa9beaf9ce7c1291a
wxxk <dwde22@gmail.com>
2023-04-14 18:32:00 +0900

    8.10 타입변환 - 강제 타입 변환

---
+++ thisisjava/src/ch08/sec10/exam02/Bus.java
@@ -0,0 +1,15 @@
+package ch08.sec10.exam02;
+
+public class Bus implements Vehicle {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("버스가 달립니다.");
+	}
+	
+	public void checkFare() {
+		System.out.println("승차요금을 체크합니다.");
+	}
+
+}
