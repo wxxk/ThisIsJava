102e5c4e4156edf0c34d6f8a845e980df7af8505
wxxk <dwde22@gmail.com>
2023-04-14 19:01:36 +0900

    7.11 봉인된 클래스

---
+++ thisisjava/src/ch07/sec11/Director.java
@@ -0,0 +1,10 @@
+package ch07.sec11;
+
+public class Director extends Manager {
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		System.out.println("제품을 기획합니다.");
+	}
+
+}
