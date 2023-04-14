d9bbeaed73aba7bc9bb17ff806b07f94fcb0e347
wxxk <dwde22@gmail.com>
2023-04-14 18:29:01 +0900

    8.6 정적 메소드

---
+++ thisisjava/src/ch08/sec06/RemoteControlExample.java
@@ -0,0 +1,30 @@
+package ch08.sec06;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RemoteControl rc;
+		
+		rc = new Television();
+		rc.turnOn();
+		rc.setVolume(5);
+		
+		rc.setMute(true);
+		rc.setMute(false);
+		
+		System.out.println();
+		
+		rc = new Audio();
+		rc.turnOn();
+		rc.setVolume(5);
+		
+		rc.setMute(true);
+		rc.setMute(false);
+		
+		System.out.println();
+		
+		RemoteControl.changeBattery();
+	}
+
+}
